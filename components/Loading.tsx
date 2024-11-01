a6b4d8c2657cdb89957cb4d1fadaaa74edb781cf
thedefidude18 <102023568+thedefidude18@users.noreply.github.com>
2024-11-01 23:20:48 +0100

    Update Loading.tsx

--- components/Loading.tsx
+++ components/Loading.tsx
@@ -1,35 +1,147 @@
-return (
-  <div className="relative flex justify-center items-center h-screen">
-    <Image
-      src="/project23/images/tappyloader.jpg" // Correct path for the background image
-      alt="Background"
-      fill // Replaces layout="fill" in Next.js 13+
-      style={{ objectFit: 'cover', objectPosition: 'center' }}
-      className="z-0" // Keeps background behind other content
-    />
+'use client'
 
-    <div className="w-full max-w-xl text-white flex flex-col items-center z-10">
-      <div className="w-64 h-64 rounded-full circle-outer p-2 mb-8">
-        <div className="w-full h-full rounded-full circle-inner overflow-hidden relative">
+import { useEffect, useState, useRef, useCallback } from 'react';
+import Image from 'next/image';
+import { botUrlQr, loading, main_character } from '@/images';
+import { calculateEnergyLimit, calculateLevelIndex, calculatePointsPerClick, calculateProfitPerHour, GameState, InitialGameState, useGameStore } from '@/utils/game-mechanics';
+import UAParser from 'ua-parser-js';
+import { ALLOW_ALL_DEVICES } from '@/utils/consts';
+
+interface LoadingProps {
+  setIsInitialized: React.Dispatch<React.SetStateAction<boolean>>;
+  setCurrentView: (view: string) => void;
+}
+
+export default function Loading({ setIsInitialized, setCurrentView }: LoadingProps) {
+  const initializeState = useGameStore((state: GameState) => state.initializeState);
+  const [isDataLoaded, setIsDataLoaded] = useState(false);
+  const openTimestampRef = useRef(Date.now());
+  const [isAppropriateDevice, setIsAppropriateDevice] = useState(true);
+
+  const fetchOrCreateUser = useCallback(async () => {
+    try {
+      let initData, telegramId, username, telegramName, startParam;
+
+      if (typeof window !== 'undefined') {
+        const WebApp = (await import('@twa-dev/sdk')).default;
+        WebApp.ready();
+        initData = WebApp.initData;
+        telegramId = WebApp.initDataUnsafe.user?.id.toString();
+        username = WebApp.initDataUnsafe.user?.username || 'Unknown User';
+        telegramName = WebApp.initDataUnsafe.user?.first_name || 'Unknown User';
+
+        startParam = WebApp.initDataUnsafe.start_param;
+      }
+
+      const referrerTelegramId = startParam ? startParam.replace('kentId', '') : null;
+
+      if (process.env.NEXT_PUBLIC_BYPASS_TELEGRAM_AUTH === 'true') {
+        initData = "temp";
+      }
+      const response = await fetch('/api/user', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          telegramInitData: initData,
+          referrerTelegramId,
+        }),
+      });
+      if (!response.ok) {
+        throw new Error('Failed to fetch or create user');
+      }
+      const userData = await response.json();
+
+      // Create the game store with fetched data
+      const initialState: InitialGameState = {
+        userTelegramInitData: initData,
+        userTelegramName: telegramName,
+        lastClickTimestamp: userData.lastPointsUpdateTimestamp,
+        gameLevelIndex: calculateLevelIndex(userData.points),
+        points: userData.points,
+        pointsBalance: userData.pointsBalance,
+        unsynchronizedPoints: 0,
+        multitapLevelIndex: userData.multitapLevelIndex,
+        pointsPerClick: calculatePointsPerClick(userData.multitapLevelIndex),
+        energy: userData.energy,
+        maxEnergy: calculateEnergyLimit(userData.energyLimitLevelIndex),
+        energyRefillsLeft: userData.energyRefillsLeft,
+        energyLimitLevelIndex: userData.energyLimitLevelIndex,
+        lastEnergyRefillTimestamp: userData.lastEnergyRefillsTimestamp,
+        mineLevelIndex: userData.mineLevelIndex,
+        profitPerHour: calculateProfitPerHour(userData.mineLevelIndex),
+        tonWalletAddress: userData?.tonWalletAddress,
+      };
+
+      initializeState(initialState);
+      setIsDataLoaded(true);
+    } catch (error) {
+      console.error('Error fetching user data:', error);
+    }
+  }, [initializeState]);
+
+  useEffect(() => {
+    const parser = new UAParser();
+    const device = parser.getDevice();
+    const isAppropriate = ALLOW_ALL_DEVICES || device.type === 'mobile' || device.type === 'tablet';
+    setIsAppropriateDevice(isAppropriate);
+
+    if (isAppropriate) {
+      fetchOrCreateUser();
+    }
+  }, []);
+
+  useEffect(() => {
+    if (isDataLoaded) {
+      const currentTime = Date.now();
+      const elapsedTime = currentTime - openTimestampRef.current;
+      const remainingTime = Math.max(3000 - elapsedTime, 0);
+
+      const timer = setTimeout(() => {
+        setCurrentView('game');
+        setIsInitialized(true);
+      }, remainingTime);
+
+      return () => clearTimeout(timer);
+    }
+  }, [isDataLoaded, setIsInitialized, setCurrentView]);
+
+  if (!isAppropriateDevice) {
+    return (
+      <div className="bg-[#000000] flex justify-center items-center h-screen">
+        <div className="w-full max-w-xl text-white flex flex-col items-center">
+          <h1 className="text-2xl font-bold mb-4">Play on your mobile</h1>
           <Image
-            src={main_character}
-            alt="Main Character"
-            fill
-            style={{
-              objectFit: 'cover',
-              objectPosition: 'center',
-              transform: 'scale(1.05) translateY(10%)',
-            }}
+            className="bg-white p-2 rounded-xl"
+            src={botUrlQr}
+            alt="QR Code"
+            width={200}
+            height={200}
           />
+          <p className="mt-4">@{process.env.NEXT_PUBLIC_BOT_USERNAME}</p>
         </div>
       </div>
+    );
+  }
 
+  return (
+    <div className="relative flex justify-center items-center h-screen">
       <Image
-        src={loading}
-        alt="Loading Icon"
-        width={100}
-        height={100}
+        src="/project23/images/tappyloader.jpg" // Background image path
+        alt="Background"
+        fill // Ensures full-screen coverage in Next.js 13+
+        style={{ objectFit: 'cover', objectPosition: 'center' }}
+        className="z-0" // Background behind other content
       />
-    </div>
-  </div>
-);
+
+      <div className="w-full max-w-xl text-white flex flex-col items-center z-10">
+        <div className="w-64 h-64 rounded-full circle-outer p-2 mb-8">
+          <div className="w-full h-full rounded-full circle-inner overflow-hidden relative">
+            <Image
+              src={main_character}
+              alt="Main Character"
+              fill
+              style={{
+                objectFit: 'cover',
+                objectPosition:
